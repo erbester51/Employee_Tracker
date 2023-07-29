53b230b45087b520d5b780246468760a14d508bd
erbester51 <55757768+erbester51@users.noreply.github.com>
2023-07-29 12:24:44 -0500

    Updated role and employee tables

--- db/schema.sql
+++ db/schema.sql
@@ -4,14 +4,30 @@ CREATE DATABASE employee_db;
 USE employee_db;
 
 CREATE TABLE department (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY;
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(30)
 );
 
 CREATE TABLE role (
-
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL,
+    department_id INT,
+    CONSTRAINT fk_department
+    FOREIGN KEY (department_id)
+    REFERENCES department(id)
 );
 
 CREATE TABLE employee (
-
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT,
+    manager_id INT,
+    CONTRAINT fk_role
+    FOREIGN KEY (role_id)
+    REFERENCES role(id)
+    CONSTRAINT fk_employee
+    FOREIGN KEY (manager_id)
+    REFERENCES employee(id)
 );
