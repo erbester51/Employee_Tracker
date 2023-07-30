d45fe3ecebfe395a4be099d2ce0fda6cf9586c88
erbester51 <55757768+erbester51@users.noreply.github.com>
2023-07-30 11:21:15 -0500

    Structured out the server file

--- server.js
+++ server.js
@@ -9,6 +9,44 @@ const app = express();
 app.use(express.urlencoded({ extended:false }));
 app.use(express.json());
 
-app.use(( req, res )) => {
+app.use(( req, res ) => {
     res.status(404).end();
+});
+
+const navigateChoices = () => {
+
 };
+
+const viewAllDepartments = () => {
+
+};
+
+const viewAllRoles = () => {
+
+};
+
+const viewAllEmployees = () => {
+
+};
+
+const addDepartment = () => {
+
+};
+
+const addRole = () => {
+
+};
+
+const addEmployee = () => {
+
+};
+
+const updateEmployee = () => {
+
+};
+
+navigateChoices();
+
+app.listen(PORT, () => {
+    console.log('Server running on port ${PORT}');
+});
