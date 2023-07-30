9204a39fad60781249d5f413cbe9e465d7ffbee0
erbester51 <55757768+erbester51@users.noreply.github.com>
2023-07-30 11:15:13 -0500

    Started server file

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const inquire = require("inquirer");
+const db = require('./db/connection');
+const mysql = require('mysql12');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.urlencoded({ extended:false }));
+app.use(express.json());
+
+app.use(( req, res )) => {
+    res.status(404).end();
+};
